7f4f245d06717292e84f7c39bd5d3175df541e4d
Max Hughes <mhughes2011@gmail.com>
2021-12-11 13:40:51 -0800

    adjusted state to be global in appjs and made feedback list

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
  * The empty arrows in the return statement are the new way to put a fragment.
  */
 
+import React, {useState} from 'react'
+
 import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from './data/FeedbackData'
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
     return (
         <>
             <Header />
             <div className="container">
-                <h1>My App</h1>
+                <FeedbackList feedback={feedback} />
             </div>
         </>
     )
