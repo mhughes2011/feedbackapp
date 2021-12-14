315dc22a8651b6fd1fe1e757a2dd8731722c2a33
Max Hughes <mhughes2011@gmail.com>
2021-12-13 17:43:33 -0800

    created a universal card component and updated all component proptypes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import FeedbackItem from './FeedbackItem'
 
@@ -14,6 +15,18 @@ function FeedbackList({feedback}) {
             ))}
         </div>
     )
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.array.isRequired
+    // Or you could do it like this
+    // feedback: PropTypes.arrayOf(
+    //     PropTypes.shape({
+    //         id: PropTypes.number.isRequired,
+    //         text: PropTypes.string.isRequired,
+    //         rating: PropTypes.number.isRequired
+    //     })
+    // )
 }
 
 export default FeedbackList
