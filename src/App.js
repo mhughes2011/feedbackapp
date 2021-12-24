b06258f15dc267bbb84cbd72919f96116a1d29a0
Max Hughes <mhughes2011@gmail.com>
2021-12-24 14:39:00 -0800

    handleSubmit functionality and tied it to app level state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
  */
 
 import React, {useState} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
@@ -12,6 +13,11 @@ import FeedbackForm from './components/FeedbackForm'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
 
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete this?')) {
@@ -23,7 +29,7 @@ function App() {
         <>
             <Header />
             <div className="container">
-                <FeedbackForm />
+                <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
             </div>
