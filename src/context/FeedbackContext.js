7591f1c86162872be6f7baedc8f32a98795ee1b8
Max Hughes <mhughes2011@gmail.com>
2021-12-29 11:40:51 -0800

    started implementing edit feedback feature and added clarifying comments to context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,21 +22,37 @@ export const FeedbackProvider = ({children}) => {
         }
     ])
 
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
+
+    // Delete a feedback
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete this?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
+    // Add a feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
+    }
+
+    // Set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
     }
     
     return <FeedbackContext.Provider value={{
         feedback: feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
