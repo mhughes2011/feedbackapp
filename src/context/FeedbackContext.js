0a9b5427b39bbb00d78ae60ae186951a79b0344e
Max Hughes <mhughes2011@gmail.com>
2022-01-03 20:46:28 -0800

    added delete and update feedback connectivity to db

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -23,15 +23,29 @@ export const FeedbackProvider = ({children}) => {
     }
 
     // Delete a feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to delete this?')) {
+            await fetch(`/feedback/${id}`, {
+                method: 'DELETE'
+            })
+
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
     // Update Feedback Item
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json()
+        
+        setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item))
     }
 
     // Add a feedback
